267e85083db3cc9e74262f973429cb578d824678
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-09-25 15:20:47 +0600

    sedate meee

--- InventorySystem/src/test/java/InventorySystemTest.java
+++ InventorySystem/src/test/java/InventorySystemTest.java
@@ -1,7 +1,14 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
 
 public class InventorySystemTest {
 
@@ -20,21 +27,205 @@ public class InventorySystemTest {
         inventorySystem.addProduct(product3);
     }
 
+    @AfterEach
+    void afterEach() {
+        this.inventorySystem = null;
+    }
+
     @Test
-    public void testRemoveProduct() {
-        // Verify that the product "Gadget" is removed
+    @DisplayName("removeProduct() TID:3 --> Item at Index 1")
+    public void testRemoveProductFoundAtIndex1() {
+        beforeEach();
+
         inventorySystem.removeProduct("Gadget");
         assertNull(inventorySystem.findProductByName("Gadget"));
 
-        // Verify that the product list size is reduced by 1
         int currentInventorySize = inventorySystem.getAllProducts().size();
         assertEquals(2, currentInventorySize);
 
-        // Verify that a non-existent product does not cause an issue
-        inventorySystem.removeProduct("NonExistentProduct");
-        assertEquals(2, currentInventorySize);
+        afterEach();
     }
 
-    // Other test methods...
+    @Test
+    @DisplayName("removeProduct() TID:2 --> Item at Index 0")
+    public void testRemoveProductFoundAtIndex0() {
+        beforeEach();
 
+        inventorySystem.removeProduct("Widget");
+        assertNull(inventorySystem.findProductByName("Widget"));
+
+        int currentInventorySize = inventorySystem.getAllProducts().size();
+        assertEquals(2, currentInventorySize);
+
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("removeProduct() TID:1 --> Inventory size is 0")
+    public void testRemoveProductForEmptyInventory() {
+        inventorySystem = new InventorySystem();
+
+        inventorySystem.removeProduct("Widget");
+        assertNull(inventorySystem.findProductByName("Widget"));
+
+        int currentInventorySize = inventorySystem.getAllProducts().size();
+        assertEquals(0, currentInventorySize);
+
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("calculateTotalValue() TID:1 --> Inventory size is 0")
+    public void testCalculateTotalValueForEmptyInventory() {
+        inventorySystem = new InventorySystem();
+
+        double totalValue = inventorySystem.calculateTotalValue();
+        assertEquals(0.0, totalValue);
+
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("calculateTotalValue() TID:2 --> Inventory size is 0")
+    public void testCalculateTotalValueForFullInventory() {
+        beforeEach();
+
+        double totalValue = inventorySystem.calculateTotalValue();
+        assertEquals(1400, totalValue);
+
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("updateProductQuantityExistingProduct() TID:1 --> Inventory size is 0")
+    void testUpdateProductQuantityExistingProductWithEmptyInventory() {
+        inventorySystem = new InventorySystem();
+
+        String productName = "Widget";
+        int newQuantity = 60;
+
+        inventorySystem.updateProductQuantity(productName, newQuantity);
+
+        Product updatedProduct = inventorySystem.findProductByName(productName);
+        assertEquals(newQuantity, updatedProduct.getQuantity());
+
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("updateProductQuantityExistingProduct() TID:2 --> Inventory size is 3")
+    void testUpdateProductQuantityOfProductWithFullInventory() {
+        beforeEach();
+        String productName = "Widget";
+        int newQuantity = 60;
+
+        inventorySystem.updateProductQuantity(productName, newQuantity);
+
+        Product updatedProduct = inventorySystem.findProductByName(productName);
+        assertEquals(newQuantity, updatedProduct.getQuantity());
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("updateProductQuantityExistingProduct() TID:3 --> Inventory size is 1")
+    void updateProductQuantityExistingProductWithOneProduct() {
+        this.inventorySystem = new InventorySystem();
+        this.inventorySystem.addProduct(new Product("Widget", 10.0, 50, "Vendor1"));
+
+        String productName = "Widget";
+        int newQuantity = 60;
+
+        inventorySystem.updateProductQuantity(productName, newQuantity);
+
+        Product updatedProduct = inventorySystem.findProductByName(productName);
+        assertEquals(newQuantity, updatedProduct.getQuantity());
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("GetLowStockProducts() TID:1 --> Inventory size is 2~3")
+    void testGetLowStockProducts() {
+        beforeEach();
+        int threshold = 40;
+        List<Product> lowStockProducts = inventorySystem.getLowStockProducts(threshold);
+
+        assertEquals(2, lowStockProducts.size());
+
+        for (Product product : lowStockProducts) {
+            assertTrue(product.getQuantity() < threshold);
+        }
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("GetLowStockProducts() TID:1 --> Inventory size is 0")
+    void testGetLowStockProductsFromEmptyInventory() {
+        this.inventorySystem = new InventorySystem();
+
+        int threshold = 40;
+        List<Product> lowStockProducts = inventorySystem.getLowStockProducts(threshold);
+
+        assertEquals(0, lowStockProducts.size());
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("GetLowStockProducts() TID:1 --> Inventory size is 1")
+    void testGetLowStockProductsWithThresholdEqualInventory() {
+        this.inventorySystem = new InventorySystem();
+        this.inventorySystem.addProduct(new Product("Widget", 10.0, 50, "Vendor1"));        
+        int threshold = 50;
+        List<Product> lowStockProducts = inventorySystem.getLowStockProducts(threshold);
+
+        assertEquals(1, lowStockProducts.size());
+        afterEach();
+    }
+
+    // Latest
+    @Test
+    @DisplayName("FindProductByName() TID:1 --> Inventory size is 0")
+    void testFindProductByNameWithEmptyInventory() {
+        String productName = "Widget";
+        this.inventorySystem = new InventorySystem();
+
+        Product foundProduct = inventorySystem.findProductByName(productName);
+        assertNull(foundProduct);
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("FindProductByName() TID:1 --> Inventory size is 3, Found")
+    void testFindProductByNameWithSize2() {
+        beforeEach();
+        String productName = "Widget";
+        Product foundProduct = inventorySystem.findProductByName(productName);
+        assertEquals("Widget", foundProduct.getName());
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("FindProductByName() TID:2 --> Inventory size is 3, Found")
+    void testFindProductByNameWithSize3() {
+        beforeEach();
+        String productName = "uwu";
+        Product foundProduct = inventorySystem.findProductByName(productName);
+        assertNull(foundProduct);
+        afterEach();
+    }
+
+    @Test
+    @DisplayName("AdjustAllPrices() TID:1 --> Inventory size is 3, Found")
+    void testAdjustAllPricesWithNegativePercentageIncrease() {
+        beforeEach();
+        double percentageIncrease = -5.0;
+        inventorySystem.adjustAllPrices(percentageIncrease);
+
+        // Verify that all prices have been decreased by the specified percentage
+        for (Product product : inventorySystem.getAllProducts()) {
+            double originalPrice = product.getPrice() / (1 - percentageIncrease / 100);
+            double adjustedPrice = product.getPrice();
+            assertEquals(originalPrice * (1 - percentageIncrease / 100), adjustedPrice, 0.001);
+        }
+        afterEach();
+    }
 }
